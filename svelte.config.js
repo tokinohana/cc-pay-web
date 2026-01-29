73659a943d4fa4990f8910c231f17d10bf88b98d
tokinohana <kisetsuben@gmail.com>
2026-01-29 19:44:22 +0700

    vercel SSR adaption & npm package funding

--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-		// csp: {
-		// 	directives: {
-		// 		'style-src': ['https://accounts.google.com/gsi/style', 'https://fonts.googleapis.com'],
-		// 		'font-src': ['unsafe-inline', 'https://fonts.gstatic.com']
-		// 	}
-		// },
-		// csrf: {
-		// 	checkOrigin: false, // <-- REMOVE IN PRODUCTION
-		// 	trustedOrigins: ['https://accounts.google.com']
-		// }
+		adapter: adapter()
 	}
 };
 
